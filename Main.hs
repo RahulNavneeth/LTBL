88fc984b3a09d3c6836ae954b77049b68b50f574
RahulNavneeth <rahulmnavneeth@gmail.com>
2025-04-24 18:46:43 +0530

    [chore] flatten scene array

--- Main.hs
+++ Main.hs
@@ -2,8 +2,7 @@ import Data.List (intercalate)
 
 type Pixel = Int
 newtype RGB = RGB (Int, Int, Int) deriving Show
--- TODO: Flatten the scene -> [width * height] | (i:row * width + j:col)
-type Scene = [[RGB]]
+type Scene = [RGB]
 
 data Dimension = Dimension {
     width :: Pixel,
@@ -11,7 +10,7 @@ data Dimension = Dimension {
 } deriving Show
 
 sceneWidth :: Pixel
-sceneWidth = 1920
+sceneWidth = 1920 
 
 sceneHeight :: Pixel
 sceneHeight = 1080
@@ -19,34 +18,31 @@ sceneHeight = 1080
 dimension :: Dimension
 dimension = Dimension sceneWidth sceneHeight
 
-
 outputPath :: FilePath
 outputPath = "./scene.ppm"
 
-
-toMultilineString :: Scene -> String
-toMultilineString = intercalate "\n" . map (unwords . map rgbToString)
-  where
-    rgbToString (RGB (a, b, c)) = unwords (map show [a, b, c])
+getIndex :: Int -> Int -> Int
+getIndex i j = i * width dimension + j
 
 writeAsPMM :: Scene -> IO ()
 writeAsPMM scene = writeFile outputPath $
                              "P3\n"
-                             ++ show (width dimension) ++ " " ++ show (height dimension) ++ "\n" ++
-                             "255\n" ++
-                             toMultilineString scene
+                             ++ dimension_string
+                             ++ "\n255\n" 
+                             ++ intercalate "\n" (map value scene)
+                                where
+                                    value (RGB (a, b, c)) = unwords (map show [a, b, c])
+                                    dimension_string = unwords (map show [width dimension, height dimension])
 
 generateScene :: Scene
 generateScene =
-  [[ value i j
-    | j <- [0 .. width dimension - 1] ]
-    | i <- [height dimension - 1, height dimension - 2 .. 0] ]
-  where
-    value i j = RGB (ir, ig, ib)
-      where
-        ir = floor $ 255 * fromIntegral j / fromIntegral (width dimension)
-        ig = floor $ 255 * fromIntegral i / fromIntegral (height dimension)
-        ib = floor $ 255 * 0.2
+     [value i j | i <- [0 .. height dimension - 1],  j <- [0 .. width dimension - 1]]
+     where
+       value i j = RGB (ir, ig, ib)
+         where
+           ir = floor $ 255 * fromIntegral j / fromIntegral (width dimension)
+           ig = floor $ 255 * fromIntegral i / fromIntegral (height dimension)
+           ib = floor $ 255 * 0.2
 
 main :: IO ()
 main = 
